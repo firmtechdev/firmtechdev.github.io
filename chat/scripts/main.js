9895abbd3140f52a3c672803ec1928b12c5dd7c6
fariskh <muhammedfariskh@gmail.com>
2024-09-19 22:47:41 +0530

    Arranged functions

--- chat/scripts/main.js
+++ chat/scripts/main.js
@@ -1,16 +1,25 @@
+console.log("here");
 const db = firebase.firestore();
 console.log(db);
 
 
-function fetch_collection(collection) {
-    db.collection(collection).get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
-            return "hai";
-        });
-    });
-    return "hello";
+async function fetch_collection(collection) {
+    const querySnapshot = await db.collection(collection).get();
+    const results = [];
+    querySnapshot.forEach((doc) => {
+        results.push({ id: doc.id, data: doc.data() });
+    })
+    return results;
 }
 
+async function add_collection(collection, data) {
+    await addDoc(collection(db, collection), data);
+    return true;
+}
 
-console.log(fetch_collection("messages"));
+var data = await fetch_collection("messages");
+console.log(JSON.stringify(data));
+// add_collection("messages", {""})
+
+console.log("done");
+
