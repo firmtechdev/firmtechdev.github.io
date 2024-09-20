d4e7e858c8805591d8b35f39cc73c1c344bc2bf9
rishad <rishadpulikkodan@gmail.com>
2024-09-20 18:55:08 +0530

    bux fixxes in login-module -chat app

--- chat/scripts/login.js
+++ chat/scripts/login.js
@@ -2,11 +2,19 @@ if (sessionStorage.getItem("user_id") !== null) {
 	window.location.href = "index.html";
 }
 
+function load(){
+	$("#progress-bar").css("width", "0%");
+	$(".login-left-div").css("opacity", "1");
+	$(".login-left-div").css("transform", "scale(100%)");
+}
+
 function view_signup(){
 	$(".login-display-visible").css("opacity", "0");
 	$(".signup-display-visible").css("opacity", "0");
 	$(".login-card").css("transform", "rotateY(180deg) scale(125%)");
+	$(".login-card").css("height", "377px");
 	$(".signup-card").css("transform", "rotateY(0deg) scale(125%)");
+	$(".signup-card").css("height", "377px");
 	setTimeout(()=>{
 		$(".login-body").css("background", "linear-gradient(45deg, transparent, #6200ee40)");
 		$(".login-card").css("transform", "rotateY(180deg) scale(100%)");
@@ -15,13 +23,17 @@ function view_signup(){
 	setTimeout(()=>{
 		$(".signup-display-visible").css("opacity", "1");
 	},500);
+	clear_feilds();
+	validate_signup();
 }
 
 function goto_login(){
 	$(".login-display-visible").css("opacity", "0");
 	$(".signup-display-visible").css("opacity", "0");
 	$(".login-card").css("transform", "rotateY(0deg) scale(125%)");
+	$(".login-card").css("height", "282px");
 	$(".signup-card").css("transform", "rotateY(180deg) scale(125%)");
+	$(".signup-card").css("height", "282px");
 	setTimeout(()=>{
 		$(".login-body").css("background", "linear-gradient(315deg, transparent, #6200ee40)");
 		$(".login-card").css("transform", "rotateY(0deg) scale(100%)");
@@ -30,6 +42,23 @@ function goto_login(){
 	setTimeout(()=>{
 		$(".login-display-visible").css("opacity", "1");
 	},500);
+	clear_feilds();
+	validate_login();
+}
+
+function clear_feilds(){
+	$(".mdc-text-field-helper-text").css("opacity", "0");
+	$(".mdc-text-field").css("box-shadow", "none");
+	// $("#username").reset();
+	$("#username").val(null);
+	// $("#password").reset();
+	$("#password").val(null);
+	// $("#create-username").reset();
+	$("#create-username").val(null);
+	// $("#create-password").reset();
+	$("#create-password").val(null);
+	// $("#confirm-password").reset();
+	$("#confirm-password").val(null);
 }
 
 function validate_login(){
@@ -46,15 +75,73 @@ function validate_login(){
 function validate_signup(){
 	username = $("#create-username").val().trim();
 	password = $("#create-password").val().trim();
-	if(username === "" || password === ""){
+	confirm_password = $("#confirm-password").val().trim();
+	if(username === ""){
 		$("#create-user-btn").attr("disabled", "disabled");
+		$("#create-username-label").css("box-shadow", "none");
+		$("#create-username-helper").css("color", "transparent !important");
+		$("#create-username-helper").css("opacity", "0 !important");
 	}
 	else{
-		$("#create-user-btn").removeAttr("disabled");
+		$("#progress-bar").css("width", "100%");
+		const usersRef = db.collection('users');
+		fetch_collection("users").then((users) => {
+			const foundUser = users.find(user => user.username === username);
+			if(!foundUser) {
+				$("#create-username-label").css("box-shadow", "green 0px 0px 0px 2px");
+				$("#create-username-helper").css("color", "transparent !important");
+				$("#create-username-helper").css("opacity", "0 !important");
+				$("#create-username-helper").html("");
+				if(password === "" && confirm_password === ""){
+					$("#create-user-btn").attr("disabled", "disabled");
+				}
+				else if(password === "" || confirm_password === "") {
+					$("#create-user-btn").attr("disabled", "disabled");
+				}
+				else if(password != confirm_password) {
+					$("#create-user-btn").attr("disabled", "disabled");
+					$("#create-password-label").css("box-shadow", "red 0px 0px 0px 2px");
+					$("#confirm-password-label").css("box-shadow", "red 0px 0px 0px 2px");
+					$("#create-password-helper, #confirm-password-helper").css("opacity", "1");
+					$("#create-password-helper, #confirm-password-helper").html("password not mach");
+				} else {
+					$("#create-user-btn").removeAttr("disabled");
+					$("#create-password-label").css("box-shadow", "none");
+					$("#confirm-password-label").css("box-shadow", "none");
+					$("#create-password-helper, #confirm-password-helper").css("opacity", "0");
+				}
+			}
+			else{
+				$("#create-user-btn").attr("disabled", "disabled");
+				$("#create-username-label").css("box-shadow", "red 0px 0px 0px 2px");
+				$("#create-username-helper").html("'"+username+"' is not available");
+				$("#create-username-helper").css("color", "red");
+				$("#create-username-helper").css("opacity", "1");
+			}
+			$("#progress-bar").css("width", "0%");
+		});
+	};
+	if(password === "" && confirm_password === ""){
+		$("#create-user-btn").attr("disabled", "disabled");
+	}
+	else if(password === "" || confirm_password === "") {
+		$("#create-user-btn").attr("disabled", "disabled");
+	}
+	else if(password != confirm_password) {
+		$("#create-user-btn").attr("disabled", "disabled");
+		$("#create-password-label").css("box-shadow", "red 0px 0px 0px 2px");
+		$("#confirm-password-label").css("box-shadow", "red 0px 0px 0px 2px");
+		$("#create-password-helper, #confirm-password-helper").css("opacity", "1");
+		$("#create-password-helper, #confirm-password-helper").html("password not mach");
+	} else {
+		$("#create-password-label").css("box-shadow", "none");
+		$("#confirm-password-label").css("box-shadow", "none");
+		$("#create-password-helper, #confirm-password-helper").css("opacity", "0");
 	}
 }
 
 function create_user(){
+	$("#progress-bar").css("width", "100%");
 	const usersRef = db.collection('users');
 	username = $("#create-username").val().trim();
 	password = CryptoJS.MD5($("#create-password").val().trim()).toString(CryptoJS.enc.Hex);
@@ -71,6 +158,8 @@ function create_user(){
 			usersRef.add(userData)
 			.then(docRef => {
 				console.log('User created ID: ', docRef.id);
+				goto_login();
+				$("#progress-bar").css("width", "0%");
 			})
 			.catch(error => {
 				console.error('Error creating user: ', error);
@@ -80,6 +169,10 @@ function create_user(){
 }
 
 function login(){
+	$("#progress-bar").css("width", "100%");
+	$("#username-label").css("box-shadow", "none");
+	$("#password-label").css("box-shadow", "none");
+	$(".mdc-text-field-helper-text").css("opacity", "0");
 	username = $("#username").val().trim();
 	password = CryptoJS.MD5($("#password").val().trim()).toString(CryptoJS.enc.Hex);
 	fetch_collection("users").then((users) => {
@@ -89,7 +182,25 @@ function login(){
 	     	sessionStorage.setItem("user_id", foundUser.id);
 	     	window.location.href = "index.html";
 	    } else {
-	     	console.log("User not found");
+	    	const foundUsername = users.find(user => user.username === username);
+	    	if(foundUsername && foundUsername.username === username){
+	    		$("#username-label").css("box-shadow", "green 0px 0px 0px 2px");
+	    	}
+	    	else{
+	    		$("#username-label").css("box-shadow", "red 0px 0px 0px 2px");
+	    		$("#username-helper").html("'"+username+"' is not a valid");
+	    		$("#username-helper").css("opacity", "1");
+	    	}
+	    	const foundPassword = users.find(user => user.password === password);
+	    	if(foundPassword && foundPassword.password === password){
+	    		// $("#password-label").css("box-shadow", "green 0px 0px 0px 2px");
+	    	}
+	    	else{
+	    		$("#password-label").css("box-shadow", "red 0px 0px 0px 2px");
+	    		$("#password-helper").html("incurrect password");
+	    		$("#password-helper").css("opacity", "1");
+	    	}
+	    	$("#progress-bar").css("width", "0%");
 	    }
  	});
 }
