58ce8e5be05ff280118e165b67e279715d74428e
fariskh <muhammedfariskh@gmail.com>
2024-11-08 23:17:41 +0530

    Added some UI fixes

--- scripts/chat-window.js
+++ scripts/chat-window.js
@@ -1,17 +1,17 @@
 if (sessionStorage.getItem("user_id") == null) {
-    window.location.href = "login.htm";
+	window.location.href = "login.htm";
 }
 
 var receiver_id = sessionStorage.getItem("receiver_id");
 // var receiver_id = 'vW4FLo7CssZiypk9ehsp'; //faris
 // var receiver_id = 'ozOjcnfHklpy1ajHkdik'; //rishad
 
-window.load = ()=> {
+window.load = () => {
 	$("body").css("margin-left", "0vw");
 	// pogress_end();
 }
 
-window.set_chat = async ()=> {
+window.set_chat = async () => {
 	const sender = await fetch_collection_by_filter("users", { 'id': receiver_id });
 	var user = await fetch_collection_by_filter("users", { 'id': sessionStorage.getItem("user_id") });
 	$("#user").html("logined by " + user.username);
@@ -21,46 +21,46 @@ window.set_chat = async ()=> {
 
 var prev_response = false;
 
-window.set_messages = async ()=> {
-	var out_messages = await fetch_collections_by_multi_filter("messages", "receiver_id", receiver_id, "sender_id", sessionStorage.getItem("user_id") );
-	var in_messages = await fetch_collections_by_multi_filter("messages", "receiver_id", sessionStorage.getItem("user_id"), "sender_id", receiver_id );
+window.set_messages = async () => {
+	var out_messages = await fetch_collections_by_multi_filter("messages", "receiver_id", receiver_id, "sender_id", sessionStorage.getItem("user_id"));
+	var in_messages = await fetch_collections_by_multi_filter("messages", "receiver_id", sessionStorage.getItem("user_id"), "sender_id", receiver_id);
 	var messages = [...out_messages, ...in_messages];
 	messages.sort((a, b) => {
-    	return a.data.timestamp - b.data.timestamp; 
+		return a.data.timestamp - b.data.timestamp;
 	});
 	var lastMessageId = messages.length > 0 ? messages[messages.length - 1].id : null;
 	if (prev_response !== lastMessageId) {
 		progress_start();
 		$("#messages").empty();
 		messages.forEach((message) => {
-			if (message.data.sender_id === sessionStorage.getItem("user_id")){
+			if (message.data.sender_id === sessionStorage.getItem("user_id")) {
 				var css_class = 'out-msg';
 			} else {
 				var css_class = 'in-msg';
 			};
 			$("#messages").append(
-				"<div class='msg " + css_class + " mdc-ripple-surface' id='" + message.id + "'>" + 
-					message.data.message + 
+				"<div class='msg " + css_class + " mdc-ripple-surface' id='" + message.id + "'>" +
+				message.data.message +
 				"</div>"
 			);
 		});
 		$(".msg").css("transform", "scale(1)");
 		prev_response = lastMessageId;
-		$('html,body').animate({scrollTop: $('.textarea--fixed-adjust').offset().top},'slow');
+		$('html,body').animate({ scrollTop: $('.textarea--fixed-adjust').offset().top }, 'slow');
 		mdc_and_rippls();
 		pogress_end();
 	}
 }
 
-window.back = ()=> {
+window.back = () => {
 	progress_start();
 	$("body").css("margin-left", "110vw");
-	setTimeout(()=>{
+	setTimeout(() => {
 		window.location.href = "index.html";
-	},350);
+	}, 350);
 }
 
-window.validate_msg = ()=> {
+window.validate_msg = () => {
 	var msg = document.getElementById("send-msg-text");
 	var rows_count = msg.value.split("\n").length;
 	if (rows_count > 1) {
@@ -89,47 +89,47 @@ window.validate_msg = ()=> {
 	}
 }
 
-window.send_msg = ()=> {
+window.send_msg = () => {
 	var message = $('#send-msg-text').val().replace(/\n/g, '<br>').replace(/\s/g, '&nbsp;');
 	var timestamp = new Date().getTime();
 	$("#messages").append(
-		"<div class='msg out-msg mdc-ripple-surface' id='" + timestamp + "'>" + 
-			message + 
+		"<div class='msg out-msg mdc-ripple-surface' id='" + timestamp + "'>" +
+		message +
 		"</div>"
 	);
-    $('html,body').animate({scrollTop: $('.textarea--fixed-adjust').offset().top},'slow');
-    $("#"+timestamp).css("transform", "scale(1)");
+	$('html,body').animate({ scrollTop: $('.textarea--fixed-adjust').offset().top }, 'slow');
+	$("#" + timestamp).css("transform", "scale(1)");
 	var data = {
-		message:     message,
+		message: message,
 		receiver_id: receiver_id,
-		sender_id:   sessionStorage.getItem("user_id"),
-		status:      "sent",
-		timestamp:   timestamp
+		sender_id: sessionStorage.getItem("user_id"),
+		status: "sent",
+		timestamp: timestamp
 	};
 	add_collection("messages", data);
 	$('#send-msg-text').val(null);
-    validate_msg();
+	validate_msg();
 	mdc_and_rippls();
 }
 
 window.logout = function () {
-    sessionStorage.removeItem("user_id");
-    window.location.href = "login.htm";
+	sessionStorage.removeItem("user_id");
+	window.location.href = "login.htm";
 }
 
-window.test_msg = ()=>{
+window.test_msg = () => {
 	var timestamp = new Date().getTime();
 	$("#messages").append(
-		"<div class='msg in-msg mdc-ripple-surface' id='" + timestamp + "'>"+
-			"test&nbsp;message<br>&#128512;<br>message&nbsp;test"+
+		"<div class='msg in-msg mdc-ripple-surface' id='" + timestamp + "'>" +
+		"test&nbsp;message<br>&#128512;<br>message&nbsp;test" +
 		"</div>"
 	);
-    $('html,body').animate({scrollTop: $('.textarea--fixed-adjust').offset().top},'slow');
-    $("#"+timestamp).css("transform", "scale(1)");
+	$('html,body').animate({ scrollTop: $('.textarea--fixed-adjust').offset().top }, 'slow');
+	$("#" + timestamp).css("transform", "scale(1)");
 	mdc_and_rippls();
 }
 
-function mdc_and_rippls(){
+function mdc_and_rippls() {
 	var fabs = document.querySelectorAll('.mdc-fab');
 	fabs.forEach(button => {
 		mdc.ripple.MDCRipple.attachTo(button);
@@ -151,18 +151,26 @@ function mdc_and_rippls(){
 	});
 }
 
-window.progress_start = ()=> {
+window.progress_start = () => {
 	$("#progress-bar").css("left", "0px");
 	$("#progress-bar").css("right", "auto");
 	$("#progress-bar").css("width", "100%");
 }
 
-window.pogress_end = ()=> {
+window.pogress_end = () => {
 	$("#progress-bar").css("left", "auto");
 	$("#progress-bar").css("right", "0px");
 	$("#progress-bar").css("width", "0%");
 }
-setInterval(()=>{set_messages()},500);
+
+$(document).on('keypress', function (e) {
+	if (e.which == 13) {
+		send_msg();
+		return false;
+	}
+});
+
+// setInterval(()=>{set_messages()},500);
 set_chat();
 set_messages();
 mdc_and_rippls();
